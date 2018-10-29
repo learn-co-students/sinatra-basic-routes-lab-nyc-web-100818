5307f00a39a70ecdffbf429b4abc830730695ecb
ihollander <ianholla@gmail.com>
2018-10-29 07:37:05 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name/?:name?' do
+    name = params['name'] || "undefined"
+    "My name is #{name}"
+  end
+
+  get '/hometown/?:hometown?' do
+    hometown = params['hometown'] || "undefined"
+    "My hometown is #{hometown}"
+  end
+
+  get '/favorite-song/?:song?' do
+    song = params['song'] || "undefined"
+    "My favorite song is #{song}"
+  end
+  
 end
